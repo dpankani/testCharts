02d3e232bd77d2ef714e799da34de90abd418da4
Dan Pankani <dpankani@geosyntec.com>
2015-09-28 17:02:33 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nscwebappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nscwebappApp
+ */
+angular.module('nscwebappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nscwebappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nscwebappApp
+ */
+angular.module('nscwebappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
